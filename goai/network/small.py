c7eb49547399b08427282b91ae77c3ee954dedf4
Jung Andy <jungj0925@gmail.com>
2023-01-12 19:05:36 -0500

    Updated for NCHW to NHWC

--- goai/network/small.py
+++ goai/network/small.py
@@ -4,20 +4,20 @@ from keras.layers.convolutional import Conv2D, ZeroPadding2D
 def layers(input_shape):
     return [
         #Using zero padding layers to enlarge input images
-        ZeroPadding2D(padding=3, input_shape=input_shape, data_format='channels_first'),
-        Conv2D(48, (7, 7), data_format='channels_first'), 
+        ZeroPadding2D(padding=3, input_shape=input_shape, data_format='channels_last'),
+        Conv2D(48, (7, 7), data_format='channels_last'), 
         Activation('relu'),
         
-        ZeroPadding2D(padding=2, data_format='channels_first'),
-        Conv2D(32, (5, 5), data_format='channels_first'), 
+        ZeroPadding2D(padding=2, data_format='channels_last'),
+        Conv2D(32, (5, 5), data_format='channels_last'), 
         Activation('relu'),
 
-        ZeroPadding2D(padding=2, data_format='channels_first'),
-        Conv2D(32, (5, 5), data_format='channels_first'), 
+        ZeroPadding2D(padding=2, data_format='channels_last'),
+        Conv2D(32, (5, 5), data_format='channels_last'), 
         Activation('relu'),
 
-        ZeroPadding2D(padding=2, data_format='channels_first'),
-        Conv2D(32, (5, 5), data_format='channels_first'), 
+        ZeroPadding2D(padding=2, data_format='channels_last'),
+        Conv2D(32, (5, 5), data_format='channels_last'), 
         Activation('relu'),
 
         Flatten(), Dense(512), Activation('relu')
